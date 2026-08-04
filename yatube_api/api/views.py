f421bcea5643b28b64be8dd996213f1b57e63648
Alevtina Muzychenko <fghbaim808@gmail.com>
2026-08-04 18:21:24 +1000

    Fix.

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -30,9 +30,7 @@ class CommentViewSet(viewsets.ModelViewSet):
         return get_object_or_404(Post, pk=post_id)
 
     def get_queryset(self):
-        post = self.get_post()
-        return post.comments.all()
+        return self.get_post().comments.all()
 
     def perform_create(self, serializer):
-        post = self.get_post()
-        serializer.save(author=self.request.user, post=post)
+        serializer.save(author=self.request.user, post=self.get_post())
